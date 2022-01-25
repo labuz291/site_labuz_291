d56f0693b2be324b94451ec661bb396296d6cad5
brykin.ilia@gmail.com <ilia.brykin@prounix.de>
2022-01-25 09:48:10 +0100

    Refactoring and .gitignore

--- js_basic/lesson1/task2.js
+++ js_basic/lesson1/task2.js
@@ -2,7 +2,7 @@ const TRAPEZOID_BASE_A = +prompt("Введите первое основание
 const TRAPEZOID_BASE_B = +prompt("Введите второе основание трапеции в см:");
 const TRAPEZOID_HEIGHT = +prompt("Введите высоту трапеции");
 
-let TRAPEZOID_AREA = ((TRAPEZOID_BASE_A + TRAPEZOID_BASE_B) / 2) * TRAPEZOID_HEIGHT;
+const TRAPEZOID_AREA = ((TRAPEZOID_BASE_A + TRAPEZOID_BASE_B) / 2) * TRAPEZOID_HEIGHT;
 
 if(Number.isNaN(TRAPEZOID_AREA)) {
   alert("Данные введены некорректно");
